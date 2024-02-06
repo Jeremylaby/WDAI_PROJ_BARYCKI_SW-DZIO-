714edc326259395eff10f1d99d909bfa91a65398
Jeremylaby <barycki@student.agh.edu.pl>
2024-02-06 18:35:23 +0100

    Zacząłem robić możliwość dodania admina przyciskiem

--- React/reactproj/src/persons/UserCard.js
+++ React/reactproj/src/persons/UserCard.js
@@ -1,7 +1,14 @@
+import photo from ".//images/photo.png"
+import "./persons.css"
 function UserCard({user}){
+    function addAdmin(){
+
+    }
     return<>
-        <div>
-            <span>{user.username}</span>
+        <div className={"users-card"}>
+            <img className={"users-photo"}  src={photo} alt={"unknown-person"}/>
+            <span>Username: {user.username}</span>
+            <div className={"user-button"} onClick={addAdmin}>Dodaj Admina</div>
         </div>
     </>
 }export default UserCard;
